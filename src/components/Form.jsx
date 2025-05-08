68277a22e3e4ef03b9a053e0b455806bdd51f9ee
LilianaJZ <ljimenezz@unal.edu.co>
2025-05-07 22:05:50 -0500

    Configuración del componente Form.jsx y sus estilos en css

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,7 +1,21 @@
 import "./Form.css";
-
-const Form = () => {
-
+//import { useNavigate } from 'react-router-dom';
+//() => navigate('/home')
+const Form = ({ onClose, onSave }) => {
+    //const navigate = useNavigate();
+    return (
+        <div className="modal-overlay">
+          <div className="modal-form">
+            <input type="text" placeholder="Nombre de la categoría" />
+            <input type="text" placeholder="Descripción (Opcional)" />
+            <div className="modal-buttons">
+              <button className="cancelar" onClick={onClose}>Cancelar</button>
+              
+              <button className="guardar" onClick={onSave}>Guardar</button>
+            </div>
+          </div>
+        </div>
+      );
 }
 
 export default Form;
